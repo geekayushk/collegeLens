dcefef2502e6b56e037f33e71da84294e4f6a10f
Gourav Mishra <gm830722@gmail.com>
2024-07-19 22:13:40 +0530

    whatsapp icon

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Register from './pages/register/Register';
 import Contact from './pages/contact/Contact';
+import Whatsapp from './components/whatsapp/Whatsapp';
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/contactUs" element={<Contact />} />
-        {/* <Route path="*" element={<NoPage />} /> */}
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/contactUs" element={<Contact />} />
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Routes>
+      </BrowserRouter>
+      <Whatsapp />
+    </>
   );
 }
 
